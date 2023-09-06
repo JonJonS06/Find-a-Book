c7760d1db434498822847f29284fa15c20fcb07a
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-09-06 11:55:24 -0400

    reworked the code and removed errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,17 +6,15 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
+        const userData = await User.findOne({ _id: context.user._id });
         return userData;
       }
       throw new AuthenticationError("Not Logged In");
     },
   },
   Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
@@ -32,16 +30,35 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (parent, args, context) => {
+    saveBook: async (
+      parent,
+      { bookId, authors, description, title, image, link },
+      context
+    ) => {
       if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: {
+              savedBooks: { bookId, authors, description, title, image, link },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+      } else {
+        throw new AuthenticationError("Could Not Delete Book");
+      }
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
           { _id: context.user._id },
           { $pull: { savedBooks: { bookId } } },
-          { new: true }
+          { new: true, runValidators: true }
         );
-        return updatedUser;
+      } else {
+        throw new AuthenticationError("You need to be logged in!");
       }
-      throw new AuthenticationError("Could Not Delete Book");
     },
   },
 };
