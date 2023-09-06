baa900c8e6c836feb94e3fa3e992fc914114a8fd
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-09-06 11:18:44 -0400

    fixed typos and missing info

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import gql from "@apollo/client";
 
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
@@ -58,14 +58,17 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
+      _id
       username
-      savedBooks
-      authors
-      description
-      title
-      bookId
-      image
-      link
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
