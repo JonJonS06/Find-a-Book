50f7e98b91e14f2a18e11db5a2398ade098590b9
JonJonS06 <124073266+JonJonS06@users.noreply.github.com>
2023-09-07 09:29:15 -0400

    added ignore client pulling my hair out

--- server/server.js
+++ server/server.js
@@ -3,45 +3,31 @@ const express = require("express");
 const path = require("path");
 
 const { ApolloServer } = require("apollo-server-express");
-const {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
-} = require("apollo-server-core");
-const http = require("http");
+
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-async function startApolloServer(typeDefs, resolvers) {
-  const app = express();
-  const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
-  });
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-
-  // if we're in production, serve client/build as static assets
 
   db.once("open", () => {
     app.listen(PORT, () => {
@@ -51,6 +37,6 @@ async function startApolloServer(typeDefs, resolvers) {
       );
     });
   });
-}
+};
 
 startApolloServer(typeDefs, resolvers);
